8e5e4ada1667e53bd6e149a5bbf68a8f60812e39
ssssangmin <ksm980609@naver.com>
2023-11-17 13:52:26 +0900

    카메라(프리팹완), 로비
    카메라 프리팹, 로비씬 z값 조정

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    private Rect screenRect = Rect.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,15 +14,20 @@ public class CameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // 11/16 수정중
-        //Cursor.lockState = CursorLockMode.Locked;
-        //GameObject player = GameObject.FindGameObjectWithTag("Player");
-
-        //if (player != null)
-        //{
-        //    Vector2 pos = Vector2.Lerp(player.transform.position, Input.mousePosition, 0.5f);
-        //    // 플레이어의 좌표와 연동
-        //    transform.position = new Vector3(pos.x, pos.y, -10);
-        //}
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            Vector2 pos = Vector2.zero;
+            Vector2 playerPos = new Vector2(player.transform.position.x,player.transform.position.y);
+            Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    
+            screenRect = new Rect(0f, 0f, Screen.width, Screen.height);
+            
+            if (screenRect.Contains(mousePos))
+            {
+                pos = Camera.main.ScreenToWorldPoint(mousePos);
+            }
+            transform.position = Vector2.Lerp(playerPos, pos, 0.5f);
+        }
     }
 }
