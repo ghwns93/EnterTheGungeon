d2a00a96d660b688195fd5e1f5c1b5accb15163a
ssssangmin <ksm980609@naver.com>
2023-11-23 13:40:40 +0900

    테이블(미완성)

---
+++ Assets/Structure/Table/Scripts/TableController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableController : MonoBehaviour
+{
+    public string TableRight = "TableRight";
+    public string Table_ShadowRight = "Table_ShadowRight";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
