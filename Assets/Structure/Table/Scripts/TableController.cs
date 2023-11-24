14119a98d86296c4773bcb536e1daf6bb204f842
ssssangmin <ksm980609@naver.com>
2023-11-24 13:58:50 +0900

    테이블 수정중

--- Assets/Structure/Table/Scripts/TableController.cs
+++ Assets/Structure/Table/Scripts/TableController.cs
@@ -5,16 +5,36 @@ using UnityEngine;
 public class TableController : MonoBehaviour
 {
     public string TableRight = "TableRight";
+    public string TableLeft = "TableLeft";
+    public string TableUp = "TableUp";
+    public string TableDown = "TableDown";
+
     public string Table_ShadowRight = "Table_ShadowRight";
+    public string Table_ShadowLeft = "Table_ShadowLeft";
+    public string Table_ShadowUp = "Table_ShadowUp";
+    public string Table_ShadowDown = "Table_ShadowDown";
+
+    public Animator animator;
+
+    Rigidbody2D rbody;
+
+    PlayerController playerCnt;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCnt = GetComponent<PlayerController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (animator != null)
+        {
+            animator.Play("TableIdle");
+            animator.Play("Table_ShadowIdle");
+        }
+
     }
 }
