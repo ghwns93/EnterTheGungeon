011471691088284b1002d6c7b32578ecd855af07
ssssangmin <ksm980609@naver.com>
2023-12-01 07:13:54 +0900

    테이블 완성, 로비 전체 픽셀퍼유닛 재설정

--- Assets/Structure/Table/Scripts/TableController.cs
+++ Assets/Structure/Table/Scripts/TableController.cs
@@ -1,44 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class TableController : MonoBehaviour
 {
+    // 테이블 애니메이션
     public string TableIdle = "TableIdle";
     public string TableRight = "TableRight";
     public string TableLeft = "TableLeft";
     public string TableUp = "TableUp";
     public string TableDown = "TableDown";
-    public bool guardOn = false;
+
+    // 움직일 수 잇는 상태
+    public bool canMove = false;
 
     private Animator animator;
     private Rigidbody2D rbody;
 
     private GameObject player;
 
+    // 방향벡터를 구하기 위한 벡터
+    private Vector2 Vec;
+    private Vector2 direction;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rbody = GetComponent<Rigidbody2D>();
+
+        // rigidbody의 타입을 Static으로 테이블이 움직일 수 없게.
         rbody.bodyType = RigidbodyType2D.Static;
+        // 레이어 collision matrix false로 바꿔 총알레이어와 테이블 레이어가 충돌하지 않게 조정.
+        Physics.IgnoreLayerCollision(9, 10, false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 플레이어와의 각도 계산
+        // 플레이어
         player = GameObject.FindGameObjectWithTag("Player");
 
+        // 플레이어와 테이블의 위치를 받아와서 벡터를 구하기.
         Vector2 playerPos = player.transform.position;
         Vector2 tablePos = transform.position;
 
+        Vec = playerPos - tablePos;
+        float distance = Vec.magnitude;
+        // 방향 벡터 구하기.
+        direction = Vec / distance;
 
+        if (canMove)
+        {
+            rbody = GetComponent<Rigidbody2D>();
+            // 테이블을 움직일 수 있게 변경
+            rbody.bodyType = RigidbodyType2D.Dynamic;
 
-        Guard();
-        
+            // 테이블과 총알이 충돌이 가능하게 변경. (막기)
+            Physics.IgnoreLayerCollision(9, 10, true);
+        }
+
     }
-    
+
     /* 테이블 처음 상태:
         rigidbody2d static
         총알과 충돌하지 않음. Layer조정
@@ -56,23 +80,40 @@ public class TableController : MonoBehaviour
         그 후에는 e를 눌러도 아무런 작동이 되지 않게
     */
 
-    void Guard()
-    {
-        if (guardOn)
-        {
-            rbody = GetComponent<Rigidbody2D>();
-            rbody.bodyType = RigidbodyType2D.Dynamic;
-        }
-    }
-
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             if (Input.GetButton("E"))
             {
-                animator.Play("TableRight");
-                guardOn = true;
+                if (direction.x > 0.5 && direction.x <= 1)
+                {
+                    Debug.Log("좌로밀기");
+                    animator.Play("TableLeft");
+                    canMove = true;
+                }
+                else if (direction.x < -0.5 && direction.x >= -1)
+                {
+                    Debug.Log("우로밀기");
+                    animator.Play("TableRight");
+                    canMove = true;
+                }
+                else if (direction.y > 0.5 && direction.y <= 1 )
+                {
+                    Debug.Log("아래로밀기");
+                    animator.Play("TableDown");
+                    canMove = true;
+                }
+                else if (direction.y < -0.5 && direction.y >= -1)
+                {
+                    Debug.Log("위로 밀기");
+                    animator.Play("TableUp");
+                    canMove = true;
+                }
+                else
+                {
+                    return;
+                }
             }
         }
     }
